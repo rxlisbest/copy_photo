87f21ea850a1b89a31a42faf05d414a0cf8f208c
Roy <rxlisbest@gmail.com>
2018-05-04 15:44:18 +0800

    修改文件夹命名

--- copy_photo.py
+++ copy_photo.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 import os
 import shutil
+import datetime
 
+date = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 dir = "/Users/ruixinglong/Pictures/照片图库.photoslibrary/Masters"
-to_dir = '/Users/ruixinglong/Downloads/R/'
+to_dir = "/Users/ruixinglong/Downloads/%s/" % date
 
 def copy_file(dir, to_dir, ext):
     list = os.listdir(dir)
@@ -22,7 +24,7 @@ def copy_file(dir, to_dir, ext):
 
 def R(dir, to_dir, ext_arr):
     for ext in ext_arr:
-        to_dir_inner = to_dir + os.path.sep + 'R' + ext
+        to_dir_inner = to_dir + os.path.sep + ext[1:]
         to_dir_exist = os.path.exists(to_dir_inner)
         if to_dir_exist == False:
             r = os.makedirs(to_dir_inner)
